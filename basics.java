9aeb3affcaae6582e5b1887fe0d41617bed857f0
pradeep.marri@opsera.io <pradeep.marri@opsera.io>
2022-09-27 15:58:13 +0530

    3001to6000 files added

--- basics.java
+++ basics.java
@@ -8,7 +8,7 @@ public class basics {
 
     public static void main(String[] args)
     {
-        for(int i=1;i<=3000;i++) {
+        for(int i=3001;i<=6000;i++) {
             File file = new File("C:\\Users\\prade\\git\\textdocument_"+i+".txt"); //initialize File object and passing path as argument
             boolean result;
             try {
